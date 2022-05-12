28aa1589bb20de65b2aea1d4fc9593e76c59dddd
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-05-12 11:34:44 +0530

    chore: todo UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const ToDo = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input type='text' placeholder='Enter a ToDo' />
+    </form>
   );
-}
+};
 
-export default App;
+export default ToDo;
